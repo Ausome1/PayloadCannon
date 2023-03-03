858cd358544a9f88a97b376eedc048340ec3b18e
Ausome1 <Ausome1@protonmail.com>
2023-03-03 11:42:56 -0600

    Update index.php
    Updated indentation

--- cannon/index.php
+++ cannon/index.php
@@ -2,12 +2,12 @@
 ob_start();
 $header = 'HTTP/1.1 404 Not Found';
 echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-                                <html><head>
-                                <title>404 Not Found</title>
-                                </head><body>
-                                <h1>Not Found</h1>
-                                <p>The requested URL was not found on this server.</p>
-                                </body></html>';
+  <html><head>
+  <title>404 Not Found</title>
+  </head><body>
+  <h1>Not Found</h1>
+  <p>The requested URL was not found on this server.</p>
+  </body></html>';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
         $db = '../cannon.db';
