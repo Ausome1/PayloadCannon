6c1213229b9cb1e912aa63c9a745d260f70dd7e2
Ausome1 <Ausome1@protonmail.com>
2023-03-03 11:28:20 -0600

    Update index.php
    Fixed 404 and payload showing at the same time.

--- cannon/index.php
+++ cannon/index.php
@@ -1,6 +1,13 @@
 <?php
 ob_start();
 $header = 'HTTP/1.1 404 Not Found';
+echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+                                <html><head>
+                                <title>404 Not Found</title>
+                                </head><body>
+                                <h1>Not Found</h1>
+                                <p>The requested URL was not found on this server.</p>
+                                </body></html>';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
         $db = '../cannon.db';
@@ -8,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
         $search = '/^' . htmlentities($_GET['id']) . ':/';
         foreach ($lines as $line) {
                 if (preg_match($search, $line)) {
+                        ob_clean();
                         $file = explode(':', $line);
                         $payload = file_get_contents(trim('../payloads/' . $file[1]));
                         echo '<pre>' . htmlentities($payload) . '</pre>';
@@ -15,17 +23,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
                         $db_contents = str_replace($line, '', $db_contents);
                         file_put_contents($db, $db_contents);
                         $header = 'HTTP/1.1 200 OK';
-                } else {
-                        echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-                                <html><head>
-                                <title>404 Not Found</title>
-                                </head><body>
-                                <h1>Not Found</h1>
-                                <p>The requested URL was not found on this server.</p>
-                                </body></html>';
                 }
         }
-} 
+}
 
 header($header);
 header('Cache-Control: private, no-store, max-age=0, no-cache, must-revalidate, post-check=0, pre-check=0');
